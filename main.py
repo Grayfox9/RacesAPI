3624fd285b92d75421e5ba8f57c66d4d4c2d4b8d
Ingmar <100050756+Grayfox9@users.noreply.github.com>
2022-09-15 13:29:37 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, UploadFile, File, Form
+from databases import Database
+
+
+app = FastAPI()
+
+database = Database("sqlite:///Racedb.db")
+
+
+@app.on_event("startup")
+async def database_connect():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def database_disconnect():
+    await database.disconnect()
+
+
+@app.get("/Q1 Año con mas carreras")
+async def fetch_data(Cantidad_de_Resultados: int):
+    #Consulta = "SELECT * FROM Races"
+    Consulta = "SELECT Year, sum(Round) as Carreras_totales FROM Races GROUP BY Year ORDER BY Carreras_Totales DESC LIMIT {};".format(str(Cantidad_de_Resultados))
+    results = await database.fetch_all(query=Consulta)
+    return results
+
+@app.get("/Q2 Piloto con mayor cantidad de primeros puestos")
+async def fetch_data(Cantidad_de_Resultados: int):
+    #Consulta = "SELECT * FROM Races"
+    Consulta = "SELECT D.DriverRef as NombrePiloto, count(PositionOrder) as Veces1erPuesto FROM Results R JOIN Drivers D ON (R.DriverId = D.DriverId) WHERE PositionOrder = 1 GROUP BY D.DriverId ORDER BY Veces1erPuesto DESC LIMIT {};".format(str(Cantidad_de_Resultados))
+    results = await database.fetch_all(query=Consulta)
+    return results
+
+
+@app.get("/Q3 Piloto con mayor cantidad de primeros puestos")
+async def fetch_data(Cantidad_de_Resultados: int):
+    #Consulta = "SELECT * FROM Races"
+    Consulta = "SELECT C.Name as CName, count(C.CircuitId) As TimesRide FROM Races R JOIN Circuits C ON (R.CircuitId = C.CircuitId) GROUP BY C.CircuitId ORDER BY TimesRide DESC LIMIT {};".format(str(Cantidad_de_Resultados))
+    results = await database.fetch_all(query=Consulta)
+    return results
+
+
+@app.get("/Q4 Piloto con más puntos en total(America/British)")
+async def fetch_data(Cantidad_de_Resultados: int):
+    #Consulta = "SELECT * FROM Races"
+    Consulta = "SELECT sum(R.Points), R.DriverId, D.Surname, D.Forename FROM Results R JOIN Drivers D ON (R.DriverId = D.DriverId) JOIN Constructors C ON (C.ConstructorId = R.ConstructorId) WHERE C.Nationality = 'British' OR C.Nationality = 'American' GROUP BY R.DriverId ORDER BY SUM(points) DESC LIMIT {};".format(str(Cantidad_de_Resultados))
+    results = await database.fetch_all(query=Consulta)
+    return results
+
